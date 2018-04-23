9c6b61f5996ccdce0f52d168c3d4b5e57acf82fd
hackeru <hackeru@hackerus-Mac.local>
2018-04-23 10:59:41 +0300

    Initial Commit

---
+++ Simulator2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simulator2
+//
+//  Created by hackeru on 8 Iyar 5778.
+//  Copyright © 5778 hackeru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
